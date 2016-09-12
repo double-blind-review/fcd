52455497f8b681cbad8f6f307ddb5011dc6ce644
Johannes <johannes.hartmann.calw@web.de>
2016-09-12 23:38:15 +0200

    addet tests and the rest of the atxHeading parser implementation

--- artifact/src/main/scala/markdownParser/parsers.scala
+++ artifact/src/main/scala/markdownParser/parsers.scala
@@ -4,28 +4,36 @@ package markdown
 trait MarkdownParsers { self: RichParsers with MarkdownHelperfunctions =>
 
   // ATX heading
+  class Heading(o: Int, a: List[Char]) {
+    //println("Anzahl der #: " + o + "\nInhalt der Ueberschrift: " + a);
+    print("<h" + o + ">");
+    a.foreach(print);
+    print("</h" + o + ">\n");
+  }
 
-  val openingSeq: Parser[String] = repeat(' ', 0, 3) ~> repeat('#', 1, 6) <~ some(space)
-/*
-  val closingSeq =
+  lazy val atxHeading : Parser[Any] =
+    openingSeq ~ atxHeadingContent <~ closingSeq  ^^ {
+      case(o: String , a: List[Any]) => (o.length, a)
+      /* new Heading(o.length, inlineParser.parse(a).flatten)*/
+    }
+
+  lazy val openingSeq: Parser[String] =
+    repeat(' ', 0, 3) ~> repeat('#', 1, 6) <~ some(space)
+
+  lazy val closingSeq =
     (some(space) ~ many('#') ~ many(space) ~> newline) | (many(space) ~
     many('#') ~ some(space) ~> newline) | newline
 
-  val atxHeadingContent =
-    not(some(space) ~ many(any)) &> (many(no('#')) &> inlineParser) <& not(many(any) ~ some(space))
+  lazy val atxHeadingContent =
+    not(some(space) ~ many(any)) &> (many(no('#')) &> inlineParser) <&
+    not(many(any) ~ some(space))
+
+
 
   // Hier könnte ihr Markdown Inline Parser stehen
-  val inlineParser =
+  lazy val inlineParser =
     many(any)
 
-  val atxHeading : Parser[Any] =
-    openingSeq ~ atxHeadingContent <~ closingSeq  ^^ {
-      case(o: String , a: List[Any]) => (a, o)
-  }
-
-  // o = die überschriften größe
-  // a = der überschriften Text
-*/
 }
 
 object MarkdownParsers extends MarkdownParsers with RichParsers with DerivativeParsers with MarkdownHelperfunctions
--- artifact/src/test/scala/MarkdownParsersTests/MarkdownParsersTests.scala
+++ artifact/src/test/scala/MarkdownParsersTests/MarkdownParsersTests.scala
@@ -20,13 +20,116 @@ class MarkdownParserTests extends FunSpec with Matchers with CustomMatchers {
         parse(p, s) shouldBe List(result)
       }
   }
-  describe ("atxHeading parser") {
+  // ###########################################################################
+  // ######################## ATX Heading Tetsts ###############################
+// ###########################################################################
+  describe ("Simple headings:") {
     atxHeading shouldParseWith (
-      "## asdf #\n",
-      new Heading(2, List('a', 's', 'd', 'f'))
+      "# foo\n",
+      (1, List('f','o','o'))
     )
-
-    atxHeading shouldParse "## heading \n"
+    atxHeading shouldParseWith (
+      "## foo\n",
+      (2, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "### foo\n",
+      (3, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "#### foo\n",
+      (4, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "##### foo\n",
+      (5, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "###### foo\n",
+      (6, List('f','o','o'))
+    )
+  }
+  describe ("More than six # characters is not a heading:") {
+    atxHeading shouldNotParse ("####### foo\n")
+  }
+  describe ("At least one space is required between the # characters and the heading’s contents, unless the heading is empty:") {
+    atxHeading shouldNotParse ("#5 bolt\n")
+    atxHeading shouldNotParse ("#hashtag\n")
+  }
+  describe ("This is not a heading, because the first # is escaped:") {
+    atxHeading shouldNotParse ("\\## foo\n")
+  }
+  describe ("Contents are parsed as inlines:") {
+    atxHeading shouldParseWith (
+      "# foo *bar* \\*baz\\*\n",
+      (1, List('f','o','o', ' ', '*', 'b', 'a', 'r','*', ' ' , '\\', '*', 'b', 'a', 'z','\\', '*' ))
+    )
+  }
+  describe ("Leading and trailing blanks are ignored in parsing inline content:") {
+    atxHeading shouldParseWith (
+      "#                  foo                     \n",
+      (1, List('f','o','o'))
+    )
+  }
+  describe ("One to three spaces indentation are allowed:") {
+    atxHeading shouldParseWith (
+      " ### foo\n",
+      (3, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "  ## foo\n",
+      (2, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "   # foo\n",
+      (1, List('f','o','o'))
+    )
+  }
+  describe ("Four spaces are too much:") {
+    atxHeading shouldNotParse ("    # foo\n")
+  }
+  describe ("A closing sequence of # characters is optional:") {
+    atxHeading shouldParseWith (
+      "## foo ##\n",
+      (2, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "  ###   bar    ###\n",
+      (3, List('b','a','r'))
+    )
+  }
+  describe ("It need not be the same length as the opening sequence:") {
+    atxHeading shouldParseWith (
+      "# foo ##################################\n",
+      (1, List('f','o','o'))
+    )
+    atxHeading shouldParseWith (
+      "##### foo ##\n",
+      (5, List('f','o','o'))
+    )
+  }
+  describe ("Spaces are allowed after the closing sequence:") {
+    atxHeading shouldParseWith (
+      "### foo ###     \n",
+      (3, List('f','o','o'))
+    )
+  }
+  // Abweichend von der CommonMarkSpec:
+  describe ("Characters are not allowed in the closing sequence:") {
+    atxHeading shouldNotParse ("### foo ### b\n")
+  }
+  describe ("The closing sequence must be preceded by a space:") {
+    atxHeading shouldNotParse ("# foo#\n")
+  }
+  describe ("ATX headings can be empty:") {
+    atxHeading shouldParseWith (
+      "## \n",
+      (2, List())
+    )
+    atxHeading shouldParseWith (
+      "# \n",
+      (1, List())
+    )
   }
 
 
